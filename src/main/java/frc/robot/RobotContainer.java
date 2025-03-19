1f290f6c94f40a940fd1d8e61db91cf99fc0b660
0gottogo0 <0gottogo0@gmail.com>
2025-03-19 15:34:38 -0500

    Start wendsday auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -131,11 +131,8 @@ public class RobotContainer {
       .alongWith(wrist.run(
       () -> wrist.Set(PID.Wrist.kWristL4))));
 
-    NamedCommands.registerCommand("Intake", intake.runOnce(
-        () -> intake.Open(false))
-      .alongWith(new WaitCommand(0.3)
-        .andThen(elevator.runOnce(
-        () -> elevator.Set(PID.Elevator.kElevatorIntake))))
+    NamedCommands.registerCommand("Intake", elevator.runOnce(
+        () -> elevator.Set(PID.Elevator.kElevatorIntake))
       .alongWith(pivot.runOnce(
       () -> pivot.Set(PID.Pivot.kPivotIntake)))
       .alongWith(new WaitCommand(0.3)
